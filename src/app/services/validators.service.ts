d7b990b3161b87ef0c22b67597591e89c9353129
DJR <djr@gmail.com>
2023-03-14 16:24:51 -0600

    cambio fofo

--- src/app/services/validators.service.ts
+++ src/app/services/validators.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { FormControl } from '@angular/forms';
+const FORBIDDEN_HOBIES = "patear perritos";
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +10,7 @@ export class ValidatorsService {
   constructor() { }
 
   forbiddenHobbies(control: FormControl): {[s:string]: boolean} | null {
-    if (control.value.toLowerCase().trim() === "irle al america") {
+    if (control.value.toLowerCase().trim() === FORBIDDEN_HOBIES) {
       return {
         forbiddenHobbie: true
       }
